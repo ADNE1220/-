2273d28d569f10fe994e72df8bdbd8eec754cb22
ADNE1220 <andaniel1220@gmail.com>
2023-06-09 14:22:16 +0900

    커핀 그루나루

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function(){
 
+    //팝업 영역
+    $(".pop").slideDown();
+    $(".pop_inner button").click(function(){
+        $(".pop").slideUp();
+    })
+
     //메뉴 영역
     $(".sub").hide();
 
@@ -19,6 +25,19 @@ $(function(){
         autoplaySpeed: 2000,
         dots: true,
         arrows: true,
-    })
+    })//
+
+    //section4 이미지 슬라이드
+    $(".left_move").slick({
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows: false
+    })//
+
+    //section5
+    $(".section5_inner li").click(function(e){
+        e.preventDefault()
+        $(".section5_inner li").toggleClass("on");
+    })//
 
 })//js
